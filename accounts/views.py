29dee400b2362b017731ef2c958f4776ae09b472
Andrea  Piazza <andrea@andreas-mbp.attlocal.net>
2018-09-15 13:10:17 -0700

    login/signup

---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+def signup(request):
+    if request.method == 'POST':
+        #check for password matching
+        if request.POST['password1'] == request.POST['password2']:
+            #try to get a user with name and check to see 
+                try:
+                    user = User.objects.get(username=request.POST['username'])
+                    return render(request,'accounts/signup.html',{'Error': 'Username has already been taken. Please select a different username'})
+                    #create user sending data given in request
+                except User.DoesNotExist:
+                    user = User.objects.create_user(request.POST['username'], email=request.POST['email'], password=request.POST['password1'])
+                    #upon account creation, login user and prevent user from viewing admin page
+                    login(request,user)
+                    return render(request,'accounts/signup.html', {'Success':'Account successfully created'})
+        else:
+            return render(request,'accounts/signup.html', {'Error':'Passwords didn\'t match'})
+    else:
+        return render(request,'accounts/signup.html')
+
+
+def loginview(request):
+    if request.method == 'POST':
+        #check for password matching
+        user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+        if user is not None:
+            login(request, user)
+            return render(request,'accounts/login.html', {'Error':'Login Successful'})
+        else:
+            return render(request,'accounts/login.html', {'Error':'Username/Passwords didn\'t match. Please try again.'})
+    else:
+        return render(request,'accounts/login.html')
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/andrea/wdi/homework/django-reddit/redditclone/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
