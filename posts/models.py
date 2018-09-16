74e51228effaaa20a977d5649b2bc19d20bf729e
Andrea  Piazza <andrea@andreas-mbp.attlocal.net>
2018-09-15 23:18:01 -0700

    started to render on homepage

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, default='')
+    site_url = models.TextField(default='')
+    content = models.TextField(default="")
+    created_at = models.DateTimeField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(default='')
+    
+
+    # def pub_date_pretty(self):
+    #     return self.created_at.strftime('%b %e %Y')
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-#require user to be logged in
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Post
 
-#only access if user is logged in
 @login_required
 def create(request):
-    return render(request,'posts/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['url'] and request.POST['content']:
+            post = Post()
+            post.title = request.POST['title']
+            post.url = request.POST['url']
+            post.created_at = timezone.datetime.now()
+            post.user = request.user
+            post.save()
+            return redirect('home')
+        else:
+            return render(request,'posts/create.html',{'error':'Please fill out required fields marked with * to create a post.'})
+    else:
+        return render(request, 'posts/create.html')
+
+def home(request):
+    post = Post.objects.order_by('votes_total')
+    return render(request, 'posts/home.html', {'posts':post})
